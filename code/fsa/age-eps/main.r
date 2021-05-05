13d4a0997bdbb1cdb0ecfd74a641fe28e63d15c4
jessexknight <jesse.x.knight@gmail.com>
2021-05-05 14:33:50 -0400

    add code & figs: polymode age-eps approx estimation

---
+++ code/fsa/age-eps/main.r
@@ -0,0 +1,73 @@
+library('reshape2')
+library('ggplot2')
+source('../config.r')
+source('../ontario/plot.r')
+source('../ontario/data.r')
+
+plot.pmc.margins = function(pmc){
+  margins = list(
+    'Index' = rowSums,
+    'Other' = colSums,
+    'Ratio (Index / Other)' = function(x){ log(rowSums(x)/colSums(x)) }
+  )
+  c. = do.call(rbind,lapply(names(margins),function(m){
+    c.m = sapply(pmc,margins[[m]])
+    c.m. = mix.melt(c.m)
+    c.m.$margin = m
+    return(c.m.)
+  }))
+  # compare index / other margins: plot both, then ratio
+  idx.mr = c.$margin == names(margins)[3]
+  g = plot.mix(c.[!idx.mr,]) +
+      facet_grid(cols=vars(margin)) + ylab('Age') + xlab('Contact Type')
+  ggsave('../../out/fig/polymod-m.pdf',width=6,height=5)
+  g = plot.mix(c.[idx.mr,],clim=c(-3.5,+3.5)) +
+      facet_grid(cols=vars(margin)) + ylab('Age') + xlab('Contact Type')
+  ggsave('../../out/fig/polymod-mr.pdf',width=4,height=5)
+}
+get.pmc.eps = function(pmc.y,eps.y){
+  c.mi = rowSums(pmc.y)
+  c.mo = colSums(pmc.y)
+  c.r = c.mi %*% t(c.mo) / sum(c.mo)
+  c.a = diag(c.mi)
+  c.x = (1-eps.y) * c.r + (eps.y) * c.a
+  rownames(c.x) = colnames(c.x)
+  return(c.x)
+}
+get.pmc.error = function(pmc,pmc.eps){
+  pmc = pmc + 1e-6
+  return(sum(pmc * log(pmc / pmc.eps), na.rm=TRUE)) # KL-Divergence
+  # return(mean(abs(pmc-pmc.eps)))     # absolute difference # DEBUG
+  # return(mean(abs(pmc-pmc.eps)/pmc)) # relative difference # DEBUG
+}
+estimate.pmc.eps = function(pmc){
+  eps.0 = c('home'=.5,'work'=.5,'school'=.5,'transport'=.5,'leisure'=.5,'otherplace'=.5)
+  obj.fun = function(eps,aggr=TRUE) {
+    pmc.eps = mapply(get.pmc.eps,pmc,eps,SIMPLIFY=FALSE)
+    error = mapply(get.pmc.error,pmc,pmc.eps)
+    if (aggr){ return(mean(error)) } else { return(error) }
+  }
+  opt = optim(eps.0,obj.fun,method='L-BFGS-B',lower=rep(0,6),upper=rep(1,6))
+  eps.opt = opt$par
+  print(eps.opt)
+  print(obj.fun(eps.opt,aggr=FALSE))
+  return(eps.opt)
+}
+
+pmc = load.polymod.data()
+eps = estimate.pmc.eps(pmc)
+pmc.eps = mapply(get.pmc.eps,pmc,eps,SIMPLIFY=FALSE)
+pmc.sum = lapply(list(
+  'Original' = pmc,
+  'Approx'   = pmc.eps
+),function(x){ Reduce('+',x) })
+pmc.ratio = pmc.sum[[1]] / pmc.sum[[2]]
+
+figname = function(f){ root.path('out','fig','fsa','age-eps',paste0(f,'.pdf')) }
+
+g = plot.mix.vs(pmc,    clim=c(0,10)); ggsave(figname('polymod'),    width=14,height=3)
+g = plot.mix.vs(pmc.eps,clim=c(0,10)); ggsave(figname('polymod-eps'),width=14,height=3)
+g = plot.mix.vs(pmc.sum,clim=c(0,12)); ggsave(figname('polymod-vs'), width= 5,height=3)
+g = plot.mix(mix.melt(log10(pmc.ratio)),clim=c(-1,+1),cmap='cividis');
+  ggsave(figname('polymod-rs'),width=4,height=3)
+
